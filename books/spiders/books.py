3368c7a84f4b1ca35d4e5451b899f9284109cfab
jsteijn2 <50112263+jsteijn2@users.noreply.github.com>
2019-04-30 10:15:20 +0200

    Minimal books

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -6,10 +6,17 @@ class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html',
     ]
 
+
     def parse(self, response):
+        item = {}
+        product = response.css("div.product_main")
+        item["title"] = product.css("h1 ::text").extract_first()
+        yield item
+        
+'''
         for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
         next_page = response.css("li.next > a ::attr(href)").extract_first()
@@ -21,3 +28,4 @@ class BooksSpider(scrapy.Spider):
         product = response.css("div.product_main")
         item["title"] = product.css("h1 ::text").extract_first()
         yield item
+'''
