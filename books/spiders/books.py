ebc757f948dd60dc39e8d9ccdeaf8f2317353b6c
jsteijn2 <50112263+jsteijn2@users.noreply.github.com>
2019-04-30 13:03:00 +0200

    typoa

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -20,7 +20,7 @@ class BooksSpider(scrapy.Spider):
         item = {}
 
         item["title"] = response.css("h1 ::text").extract_first()
-        item["rdm"] = respons.css("a.account-button u-hide u-show-block@tablet-l-up omniture_customer_menu").extract_first()
+        item["rdm"] = response.css("a.account-button u-hide u-show-block@tablet-l-up omniture_customer_menu").extract_first()
         yield item
 
 
