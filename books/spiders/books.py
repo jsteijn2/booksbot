204be939e5940c26ff92192b13dc096184da57d4
jsteijn2 <50112263+jsteijn2@users.noreply.github.com>
2019-04-30 11:01:53 +0200

    Urls.txt? Maybes

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -5,16 +5,20 @@ import scrapy
 class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
-    start_urls = [
-        'https://www.google.com/shopping'
-    ]
-
+    try:
+        with open("urls.txt", "rt") as f:
+            start_urls = start_urls = [url.strip() for url in f.readlines()]
+    except:
+        start_urls = [
+            "www.google.com/shopping"
+        ]
 
     def parse(self, response):
         item = {}
         item["title"] = response.css("h1 ::text").extract_first()
         yield item
-        
+
+
 '''
         for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
             yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
